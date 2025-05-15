1de3bf90235f3b5eb1bcceb1cf777159fb06f36b
johjiny <jinful@naver.com>
2025-05-15 13:24:11 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+set -e
+
+# 시스템 패키지 업데이트
+apt-get update
 
 # Chrome 설치
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 # ChromeDriver 버전 맞추기
 CHROME_VERSION=$(google-chrome --version | awk '{ print $3 }' | cut -d '.' -f 1)
-wget -N https://chromedriver.storage.googleapis.com/$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)/chromedriver_linux64.zip
+CHROMEDRIVER_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
+wget -N https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 mv chromedriver /usr/local/bin/chromedriver
 chmod +x /usr/local/bin/chromedriver
